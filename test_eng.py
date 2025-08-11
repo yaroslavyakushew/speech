3ac6ca4883eb82cced28002dd4a08ba9bdba5e0e
yaroslavyakushew <rashir35228@stud.op.edu.ua>
2025-08-11 16:23:45 +0300

    POWERFULLgit add .git add .git add .git add .git add .git add .git add .!

--- test_eng.py
+++ test_eng.py
@@ -1,14 +1,32 @@
 from vosk import Model, KaldiRecognizer 
-import pyaudio, json
+import pyaudio, json, difflib
 
 grammar = '["up", "down", "front", "back", "to me", "from me"]' 
 model = Model('/home/sergey/nano eng model')
 rec = KaldiRecognizer(model, 16000, grammar)
 audio = pyaudio.PyAudio()
 
+
 CHUNK = 2048
 stream = audio.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=CHUNK)
 stream.start_stream()
+
+#Функции комманд
+def up():
+    print("up to 60 degrees")
+def down():
+    print("down to 60 degrees")
+def front():
+    print("front to 60 degrees")
+def back():
+    print("back")
+def to_me():
+    print("to me")
+def from_me():
+    print("from me")
+def stop():
+    print("stop")
+
 
 def listening():
     while True:
@@ -19,15 +37,19 @@ def listening():
             if result:
                 yield result
 
-keywords = {"up", "down", "front", "back", "to me", "from me"}
+keywords = {"up": up, "down": down, "front": front, "back": back, "to me": to_me, "from me": from_me, "stop": stop} #Сюда добавлять команды
 
 for text in listening():
     print(f"[text] {text}")
     if text in keywords:
         print(f"✅ Command recognized: {text}")
-        if text == "up":
-            pass
-        if text == "down":
+        keywords[text]()
+        
             
     else:
-        print("❌ Unrecognized or partial command.")
+        match = difflib.get_close_matches(text, n=1, cutoff=0.6)
+        if match:
+            print(f"✅ Command recognized: {text}")
+            keywords[match[0]]()
+        else:
+            print("❌ Unrecognized or partial command.")
